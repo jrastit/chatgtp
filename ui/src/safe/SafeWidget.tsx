8846175db8732627b0b141fe40987cc5af63d02a
jrastit <jerome.rastit@aitivity.com>
2023-07-21 22:49:57 +0200

    add connection info

--- ui/src/safe/SafeWidget.tsx
+++ ui/src/safe/SafeWidget.tsx
@@ -1,15 +1,28 @@
 import { Button } from "react-bootstrap"
 import { web3AuthModalPack } from "./safe"
+import { useState } from "react";
 
 const SafeWidget= () => {
 
-    const openSafe = () => {
-        web3AuthModalPack.signIn()
+    const [authKitSignData, setAuthKitSignData] = useState<any>(null)
+    
+    const openSafe = async () => {
+        const authKitSignData = await web3AuthModalPack.signIn()
+        setAuthKitSignData(authKitSignData)
     }
 
     return (
         <div>
             <Button onClick={() => openSafe()}>Login</Button>
+            {authKitSignData && authKitSignData.safes && authKitSignData.safes.map((safe: any) => {
+                return (
+                    <div key={safe.safeAddress}>
+                        <p>Safe Address: {safe.safeAddress}</p>
+                        <p>Safe Name: {safe.safeName}</p>
+                        <p>Safe Owner: {safe.safeOwner}</p>
+                    </div>
+                )
+            }
         </div>
     );
 };
