aa0531e5a69528acb66d8d30d95709a754b97962
romain <romain.rapallini@freepro.com>
2023-07-21 23:53:25 +0200

    [+] add chakra and react flow module and add component to display graph

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -3,15 +3,65 @@ import Wallet from "./wallet/Wallet";
 import MyChatbot from "./chatbot/MyChatbot";
 import SafeWidget from './safe/SafeWidget';
 
+import {  Box, ChakraProvider } from "@chakra-ui/react";
+import GraphFlow from "./GraphFlow/GraphFlow";
+
+const nodeDefaults = {
+    // sourceposition: Position.Right,  
+    // targetPosition: Position.Left,
+        style: {
+          borderRadius: '100%',
+          backgroundColor: '#fff',
+          width: 50,
+          height: 50,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+   };
+
+const nodes = [
+    {
+      id: '1',
+      data: { label: 'Hello' },
+      position: { x: 0, y: 0 },
+      ...nodeDefaults
+    },
+    {
+      id: '2',
+      data: { label: 'World' },
+      position: { x: 100, y: 100 },
+      ...nodeDefaults
+    },
+    {
+        id: '3',
+        data: { label: 'World' },
+        position: { x: 200, y: 200 },
+        ...nodeDefaults
+      },
+  ];
+
+const edges = [
+    {
+        id: 'e1-2',
+        source: '1',
+        target: '2',
+    },
+]
+
 const App: FunctionComponent = () => {
     return (
         <>
-            <MyChatbot/>
-            <p/>
-            <Wallet/>
-            <p/>
-            <SafeWidget/>
-
+        <ChakraProvider>
+            <Box display="flex">
+                <MyChatbot/>
+                <p/>
+                <Wallet/>
+                <p/>
+                <SafeWidget/>
+                <GraphFlow initialEdges={edges} initialNodes={nodes} />
+            </Box>
+        </ChakraProvider>
         </>
     );
 }
