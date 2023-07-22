b921ff6352e71d37bd58d0ec5bb240733838f0d7
Xavier Fournet <461943+xfournet@users.noreply.github.com>
2023-07-22 13:46:25 +0200

    Fix css

--- ui/src/biconomy/Biconomy.tsx
+++ ui/src/biconomy/Biconomy.tsx
@@ -1,4 +1,4 @@
-import "@Biconomy/web3-auth/dist/src/style.css"
+import '@biconomy/web3-auth/dist/src/style.css'
 import { useState, useEffect, useRef } from 'react'
 import SocialLogin from "@biconomy/web3-auth"
 import { ChainId } from "@biconomy/core-types";
