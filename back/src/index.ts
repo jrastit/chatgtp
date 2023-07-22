51c156de5fc23ffcb6a587c7745ad7085b8d8007
Xavier Fournet <461943+xfournet@users.noreply.github.com>
2023-07-22 23:50:56 +0200

    serve front

--- back/src/index.ts
+++ back/src/index.ts
@@ -5,11 +5,10 @@ import bodyParser from "body-parser";
 const port = 8080;
 const app = express();
 
-app.get('/', (_, res) => {
-    res.send('Hello');
-});
 
 app.use('/api', bodyParser.json());
 app.post('/api/chatbot', chatbot);
+
+app.get('*', express.static('../ui/build/', { cacheControl: true, maxAge: 7 * 24 * 3600 * 1000 }));
 
 app.listen(port, () => console.log(`Ready to process request on port ${port}`));
